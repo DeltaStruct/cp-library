c2ae40f7fe8a7081f9c8dff0b989a0e7d6950c4c
DeltaStruct <DeltaStruct>
2026-04-27 02:40:59 +0900

    online range row argmin query

--- monge/online_range_row_argmin.hpp
+++ monge/online_range_row_argmin.hpp
@@ -1,7 +1,97 @@
 #pragma once
 #include "assets/stdc++.hpp"
 
-template<class F>
-struct online_range_row_argmin {
-  ;
+template<class F,class C = less<>>
+requires invocable_r<bool,C,return_type(value,F),return_type(value,F)>
+struct rra_v2s1 {
+  using T = arg_type(0,value,F);
+  F f;
+  C comp;
+  bool select(T i,int k,int j){
+    return comp(f.value(i,k),f.value(i,j));
+  }
+  rra_v2s1(F _f,C _comp = C()) : f(std::move(_f)),comp(std::move(_comp)) {}
 };
+
+template<class F>
+requires invocable_r(bool,select,F,arg_type(0,select,F),int,int)
+struct online_range_row_argmin {
+  using T = arg_type(0,select,F);
+  using S = online_range_row_argmin;
+  T h; int w;
+  vector<pair<T,int>> segtree;
+  F f;
+  online_range_row_argmin(T _h,int _w,F _f) : h(_h),w(_w),segtree(w),f(std::move(_f)) {
+    build();
+  }
+  template<class V>
+  requires invocable(value,V,T,int)
+  online_range_row_argmin(T _h,int _w,V _f) : S(_h,_w,rra_v2s1(std::move(_f))) {}
+  template<class V,class C>
+  requires invocable(value,V,T,int)
+  online_range_row_argmin(T _h,int _w,V _f,C c) : S(_h,_w,rra_v2s1(std::move(_f),std::move(c))) {}
+  void build(){
+    vector<vector<pair<T,int>>> table(2*w);
+    for (int i(0);i < w;++i) table[w+i].emplace_back(h-1,i);
+    for (int t(w-1);t;--t) segtree[t] = [&](auto& X,auto& Y,auto& R){
+      for (int i(0),k(0);i < (int)X.size();){
+        if (!f.select(min(X[i],Y[k]).first,X[i].second,Y[k].second)){
+          T l = min(X[i],Y[k]).first,r = max((i==0?-1:X[i-1].first),(k==0?-1:Y[k-1].first));
+          while(l-r>1){
+            T mid = (l+r)/2;
+            if (f.select(mid,X[i].second,Y[k].second)) r = mid;
+            else l = mid;
+          }
+          l = (r!=-1&&(i==0||X[i].second!=r));
+          R.reserve(i+l+(int)Y.size()-k);
+          copy(X.begin(),X.begin()+i,back_inserter(R));
+          if (l) R.emplace_back(r,X[i].second);
+          copy(Y.begin()+k,Y.end(),back_inserter(R));
+          int t = X[i].second;
+          table.pop_back(),table.pop_back();
+          return make_pair(r,t);
+        }
+        if (X[i].first==Y[k].first) ++i,++k;
+        else if (X[i].first<Y[k].first) ++i;
+        else ++k;
+      }
+      R = X;
+      table.pop_back(),table.pop_back();
+      return R.back();
+    }(table[t<<1],table[t<<1|1],table[t]);
+  }
+  int row_argmin(T x,int l,int r){
+    assert(l<r);
+    auto solve = [&](auto& rec,int y) -> int {
+      if (y>=w) return y-w;
+      if (segtree[y].first>=x) return min(segtree[y].second,rec(rec,y<<1));
+      return rec(rec,y<<1|1);
+    };
+    int ret = l;
+    for (l+=w,r+=w;l < r;l>>=1,r>>=1){
+      if (l&1){
+        int t = solve(solve,l++);
+        if (ret<t) swap(ret,t);
+        if (f.select(x,t,ret)) ret = t;
+      }
+      if (r&1){
+        int t = solve(solve,--r);
+        if (ret<t) swap(ret,t);
+        if (f.select(x,t,ret)) ret = t;
+      }
+    }
+    return ret;
+  }
+};
+
+template<class F>
+requires invocable_r(bool,select,F,arg_type(0,select,F),int,int)
+online_range_row_argmin(arg_type(0,select,F),int,F) -> online_range_row_argmin<F>;
+
+template<class V>
+requires invocable(value,V,arg_type(0,value,V),int)
+online_range_row_argmin(arg_type(0,value,V),int,V) -> online_range_row_argmin<rra_v2s1<V>>;
+
+template<class V,class C>
+requires invocable(value,V,arg_type(0,value,V),int)
+online_range_row_argmin(arg_type(0,value,V),int,V,C) -> online_range_row_argmin<rra_v2s1<V,C>>;
---
+++ verify/joi.baker.online_range_row_argmin.cpp
@@ -0,0 +1,24 @@
+// competitive-verifier: LOCALCASE //verify/localcase/joi/baker
+#include "assets/stdc++.hpp"
+#include "monge/online_range_row_argmin.hpp"
+
+int main(){
+  ios::sync_with_stdio(false),cin.tie(0);
+  long long n,s; int m,q; cin >> n >> m >> s >> q;
+  vector<long long> A(m); for (auto& a:A) cin >> a;
+  vector<pair<long long,long long>> Q(q);
+  for (auto& [a,b]:Q) cin >> a >> b;
+  online_range_row_argmin segtree((long long)4e12,m,guide(value)(
+    [&](long long i,int k){ return A[k]-i*k; }
+  ));
+  for (auto [a,b]:Q){
+    int l = lower_bound(A.begin(),A.end(),b+a-s)-A.begin();
+    int r = upper_bound(A.begin(),A.end(),b)-A.begin();
+    if (l>=r){
+      cout << 0 << '\n';
+      continue;
+    }
+    int res = segtree.row_argmin(a,l,r);
+    cout << min((A[res]+s-b)/a+r-1-res,(long long)r-l) << '\n';
+  }
+}
